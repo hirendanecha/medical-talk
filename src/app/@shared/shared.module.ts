3a80a3a341084212d67a908e56a3e9f533cfcd85
ppa-opash <ppa.opash@gmail.com>
2024-04-03 13:06:02 +0530

    Apply Changes

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -80,6 +80,7 @@ import {
   faList,
   faCircle,
   faPhone,
+  faRefresh,
 } from '@fortawesome/free-solid-svg-icons';
 import { ClaimTokenModalComponent } from './modals/clai-1776-token-modal/claim-token-modal.component';
 import { WalletLinkComponent } from './modals/wallet-download-modal/1776-wallet.component';
@@ -221,7 +222,8 @@ export class SharedModule {
       faGlobe,
       faList,
       faCircle,
-      faPhone
+      faPhone,
+      faRefresh
     );
   }
 }
